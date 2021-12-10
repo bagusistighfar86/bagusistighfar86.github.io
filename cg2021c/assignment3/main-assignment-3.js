53f2a5f925d2b73816c6592fb222fbe1ba4ab5bf
bagusistighfar86 <79921639+bagusistighfar86@users.noreply.github.com>
2021-12-10 18:07:00 +0700

    Update main-assignment-3.js

--- cg2021c/assignment3/main-assignment-3.js
+++ cg2021c/assignment3/main-assignment-3.js
@@ -121,6 +121,56 @@ function main() {
 		}
 	`;
 
+	var fragmentShaderSourcePlane = `
+	precision mediump float;
+	varying vec3 vPosition;
+	varying vec3 vColor;
+	varying vec3 vNormal;
+	uniform vec3 uAmbientConstant;   // Represents the light color
+	uniform float uAmbientIntensity;
+	uniform vec3 uDiffuseConstant;  // Represents the light color
+	uniform vec3 uLightPosition;
+	uniform mat3 uNormalModel;
+	uniform vec3 uSpecularConstant; // Represents the light color
+	uniform vec3 uViewerPosition;
+	void main() {
+		
+		// Calculate the ambient component
+		vec3 ambient = uAmbientConstant * uAmbientIntensity;
+		
+		// Prepare the diffuse components
+		vec3 normalizedNormal = normalize(uNormalModel * vNormal);
+		vec3 vLight = uLightPosition - vPosition;
+		vec3 normalizedLight = normalize(vLight);
+		vec3 diffuse = vec3(0., 0., 0.);
+		float cosTheta = max(dot(normalizedNormal, normalizedLight), 0.);
+		// Prepare the specular components
+		vec3 vReflector = 2.0 * cosTheta * vNormal - (vLight);
+		// or using the following expression
+		// vec3 vReflector = reflect(-vLight, vNormal);
+		vec3 vViewer = uViewerPosition - vPosition;
+		vec3 normalizedViewer = normalize(vViewer);
+		vec3 normalizedReflector = normalize(vReflector);
+		float shininessConstant = 100.0;
+		vec3 specular = vec3(0., 0., 0.);
+		float cosPhi = max(dot(normalizedViewer, normalizedReflector), 0.);
+		
+		// Calculate the phong reflection effect
+		if (cosTheta > 0.) {
+			diffuse = uDiffuseConstant * cosTheta;
+		}
+		if (cosPhi > 0.) {
+			specular = uSpecularConstant * pow(cosPhi, shininessConstant);
+		}
+		vec3 phong = ambient + diffuse + specular;
+		// Apply the shading
+		gl_FragColor = vec4(phong * vColor, 1.);
+		// gl_FragColor.w = 0.5; //transparency
+	}
+	`;
+
+	var challenge4switch = false;
+
 	// copying the first obj
 	var vertices_left = new Float32Array(vertices);
 
@@ -195,7 +245,7 @@ function main() {
 		box[it + 2] += box_center[2];
 	}
 
-	var cube_box = 24 * 4;
+	var cube_box = 24 * 5;
 	var box_index = [
 		// cube light
 		0 + cube_box, 1 + cube_box, 2 + cube_box,     0 + cube_box, 2 + cube_box, 3 + cube_box,     // Face A
@@ -219,26 +269,33 @@ function main() {
 	gl.shaderSource(fragmentShaderL, fragmentShaderSourceL);
 	var fragmentShaderR = gl.createShader(gl.FRAGMENT_SHADER);
 	gl.shaderSource(fragmentShaderR, fragmentShaderSourceR);
+	var fragmentShaderPlane = gl.createShader(gl.FRAGMENT_SHADER);
+	gl.shaderSource(fragmentShaderPlane, fragmentShaderSourcePlane);
 
 
 	// Compile .c into .o
 	gl.compileShader(vertexShader);
 	gl.compileShader(fragmentShaderL);
 	gl.compileShader(fragmentShaderR);
+	gl.compileShader(fragmentShaderPlane);
 
 	// Prepare a .exe shell (shader program)
 	var shaderProgramL = gl.createProgram();
 	var shaderProgramR = gl.createProgram();
+	var shaderProgramPlane = gl.createProgram();
 
 	// Put the two .o files into the shell
 	gl.attachShader(shaderProgramL, vertexShader);
 	gl.attachShader(shaderProgramL, fragmentShaderL);
 	gl.attachShader(shaderProgramR, vertexShader);
 	gl.attachShader(shaderProgramR, fragmentShaderR);
+	gl.attachShader(shaderProgramPlane, vertexShader);
+	gl.attachShader(shaderProgramPlane, fragmentShaderPlane);
 
 	// Link the two .o files, so together they can be a runnable program/context.
 	gl.linkProgram(shaderProgramL);
 	gl.linkProgram(shaderProgramR);
+	gl.linkProgram(shaderProgramPlane);
 	// end shader init section
 
 
@@ -252,6 +309,7 @@ function main() {
 
 	var uLightPosition = gl.getUniformLocation(shaderProgramL, "uLightPosition");
 	var uLightPositionR = gl.getUniformLocation(shaderProgramR, "uLightPosition");
+	var uLightPositionPlane = gl.getUniformLocation(shaderProgramPlane, "uLightPosition");
 
 	gl.useProgram(shaderProgramL);
 	var uView = gl.getUniformLocation(shaderProgramL, "uView");
@@ -266,7 +324,6 @@ function main() {
 
 	gl.useProgram(shaderProgramR);
 	var uViewR = gl.getUniformLocation(shaderProgramR, "uView");
-	gl.uniformMatrix4fv(uViewR, false, viewMatrix);
 	var viewMatrixR = glMatrix.mat4.create();
 	glMatrix.mat4.lookAt(
 			viewMatrixR,
@@ -275,22 +332,60 @@ function main() {
 			[0.0, 1.0, 0.0]
 	);
 	gl.uniformMatrix4fv(uViewR, false, viewMatrixR);
-	// end cam init section 
 
+	gl.useProgram(shaderProgramPlane);
+	var uViewPlane = gl.getUniformLocation(shaderProgramPlane, "uView");
+	var viewMatrixPlane = glMatrix.mat4.create();
+	glMatrix.mat4.lookAt(
+			viewMatrixPlane,
+			[cameraX, cameraY, cameraZ],    // the location of the eye or the camera
+			[cameraX, 0.0, -10],        // the point where the camera look at
+			[0.0, 1.0, 0.0]
+	);
+	gl.uniformMatrix4fv(uViewPlane, false, viewMatrixPlane);
+	// end cam init section
+	
 	// controller section
-  function changeBoxPos(xyz, mov) {
-		var index_start = 9 * ( 24 * 4 + 20 );
+
+	// Interactive orbital rotation with mouse using quaternion concept
+	var rotationMatrix = glMatrix.mat4.create();
+	var lastPointOnTrackBall = currentPointOnTrackBall = getProjectionPointOnSurface(glMatrix.vec3.fromValues(cameraX, cameraY, 0));
+	var lastQuat = glMatrix.quat.create();
+	function computeCurrentQuat() {
+		// count the quartenion rotation in every change of mouse pointer position 
+		var axisFromCrossProduct = glMatrix.vec3.cross(glMatrix.vec3.create(), lastPointOnTrackBall, currentPointOnTrackBall);
+		var angleFromDotProduct = Math.acos(glMatrix.vec3.dot(lastPointOnTrackBall, currentPointOnTrackBall));
+		var rotationQuat = glMatrix.quat.setAxisAngle(glMatrix.quat.create(), axisFromCrossProduct, angleFromDotProduct);
+		glMatrix.quat.normalize(rotationQuat, rotationQuat);
+		return glMatrix.quat.multiply(glMatrix.quat.create(), rotationQuat, lastQuat);
+	}
+	// Project the mouse pointer to the surface of trackball
+	function getProjectionPointOnSurface(point) {
+		var radius = canvas.width/3;  // trackball virtual radius : 1/3 of canvas' width
+		var center = glMatrix.vec3.fromValues(canvas.width/2, canvas.height/2, 0);  // Center Point of Trackball virtual
+		var pointVector = glMatrix.vec3.subtract(glMatrix.vec3.create(), point, center);
+		pointVector[1] = pointVector[1] * (-1); // Flip the y value
+		var radius2 = radius * radius;
+		var length2 = pointVector[0] * pointVector[0] + pointVector[1] * pointVector[1];
+		if (length2 <= radius2) pointVector[2] = Math.sqrt(radius2 - length2); // Get the z value from the phytagoras formula
+		else {  // Set the value as 0, than x and y as the Phytagoras which form a slanted side along the radius					pointVector[0] *= radius / Math.sqrt(length2);
+				pointVector[1] *= radius / Math.sqrt(length2);
+				pointVector[2] = 0;
+		}
+		return glMatrix.vec3.normalize(glMatrix.vec3.create(), pointVector);
+	}
+
+  	function changeBoxPos(xyz, mov) {
+		var index_start = 9 * 24 * 5;
 		for (var it = 0; it < box.length; it += 9) {
 			vertices[index_start + it + xyz] += mov;
 		}
 	}
-
 	function lightController(event) {
 		//w
-		var index_start = 9 * 24 * 14;
 		if (event.keyCode == 87) {
-			box_center[1]+=0.1;
-			changeBoxPos(1, 0.1);
+			box_center[2]-=0.1;
+			changeBoxPos(2, -0.1);
 		} 
 		//a
 		if (event.keyCode == 65) {
@@ -299,66 +394,106 @@ function main() {
 		}
 		//s
 		if (event.keyCode == 83){
-			box_center[1]-=0.1;
-			changeBoxPos(1, -0.1);
+			box_center[2]+=0.1;
+			changeBoxPos(2, 0.1);
 		}
 		//d
 		if (event.keyCode == 68) {
 			box_center[0]+=0.1;
 			changeBoxPos(0, 0.1);
 		} 
-		//e
-		if (event.keyCode == 69) {
-			box_center[2]-=0.1;
-			changeBoxPos(2, -0.1);
-		}
-		//q
-		if (event.keyCode == 81) {
-			box_center[2]+=0.1;
-			changeBoxPos(2, 0.1);
-		}
-		if(cur_program == 'l') gl.uniform3fv(uLightPosition, box_center);
-		if(cur_program == 'l') gl.uniform3fv(uLightPositionR, box_center);
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 	}
-
+	function rotate(cx, cy, x, y, angle) {
+		var radians = (Math.PI / 180) * angle,
+			cos = Math.cos(radians),
+			sin = Math.sin(radians),
+			nx = (cos * (x - cx)) + (sin * (y - cy)) + cx,
+			ny = (cos * (y - cy)) - (sin * (x - cx)) + cy;
+		return [nx, ny];
+	}
 	function onKeydown(event) {
-			if (event.keyCode == 37) cameraX -= 0.1; // Left
-			if (event.keyCode == 38) cameraZ -= 0.1; // Up
-			if (event.keyCode == 39) cameraX += 0.1; // Right
-			if (event.keyCode == 40) cameraZ += 0.1; // Down
-			if (event.keyCode == 33) cameraY += 0.1; // Right
-			if (event.keyCode == 34) cameraY -= 0.1; // Down
-			lightController(event);
-			glMatrix.mat4.lookAt(
-					viewMatrix,
-					[cameraX, cameraY, cameraZ],    // the location of the eye or the camera
-					[cameraX, 0.0, -10],        // the point where the camera look at
-					[0.0, 1.0, 0.0]
-			);
-			glMatrix.mat4.lookAt(
-				viewMatrixR,
+		var cameraXZ;
+		if (event.keyCode == 37) {
+			cameraXZ = rotate(0.0, 0.0, cameraX, cameraZ, -5); // Left
+			cameraX = cameraXZ[0];
+			cameraZ = cameraXZ[1];
+		} 
+		if (event.keyCode == 38) cameraZ -= 0.1; // Up
+		if (event.keyCode == 39){
+			cameraXZ = rotate(0.0, 0.0, cameraX, cameraZ, 5); // Right
+			cameraX = cameraXZ[0];
+			cameraZ = cameraXZ[1];
+		} 
+		if (event.keyCode == 40) cameraZ += 0.1; // Down
+		if (event.keyCode == 33) cameraY += 0.1; // pgup
+		if (event.keyCode == 34) cameraY -= 0.1; // pgdown
+		if (event.keyCode == 32) 
+			if (challenge4switch) challenge4switch = false; // space
+			else challenge4switch = true;
+		
+		lightController(event);
+		
+		glMatrix.mat4.lookAt(
+				viewMatrix,
 				[cameraX, cameraY, cameraZ],    // the location of the eye or the camera
-				[cameraX, 0.0, -10],        // the point where the camera look at
+				[0.0, 0.0, 0],        // the point where the camera look at
 				[0.0, 1.0, 0.0]
-			);
+		);
+		glMatrix.mat4.lookAt(
+			viewMatrixR,
+			[cameraX, cameraY, cameraZ],    // the location of the eye or the camera
+			[0.0, 0.0, 0],        // the point where the camera look at
+			[0.0, 1.0, 0.0]
+		);
+		glMatrix.mat4.lookAt(
+			viewMatrixPlane,
+			[cameraX, cameraY, cameraZ],    // the location of the eye or the camera
+			[0.0, 0.0, 0],        // the point where the camera look at
+			[0.0, 1.0, 0.0]
+		);
+		// gl.uniformMatrix4fv(uView, false, viewMatrix);
+		// gl.uniformMatrix4fv(uViewR, false, viewMatrixR);
+		
+		// trackball event
+		var dragging;
+    	function onMouseDown(event) {
+			var x = event.clientX;
+			var y = event.clientY;
+			var rect = event.target.getBoundingClientRect();
+			// When the mouse pointer is inside the frame
+			if (
+				rect.left <= x &&
+				rect.right >= x &&
+				rect.top <= y &&
+				rect.bottom >= y
+			) {
+				dragging = true;
+			}
+			lastPointOnTrackBall = getProjectionPointOnSurface(glMatrix.vec3.fromValues(x, y, 0));
+			currentPointOnTrackBall = lastPointOnTrackBall;
+		}
+		function onMouseUp(event) {
+			dragging = false;
+			if (currentPointOnTrackBall != lastPointOnTrackBall) {
+				lastQuat = computeCurrentQuat();
+			}
+    	}
+		function onMouseMove(event) {
+			if (dragging) {
+				var x = event.clientX;
+				var y = event.clientY;
+				currentPointOnTrackBall = getProjectionPointOnSurface(glMatrix.vec3.fromValues(x, y, 0));
+				glMatrix.mat4.fromQuat(rotationMatrix, computeCurrentQuat());
+			}
+		}
+		document.addEventListener('mousedown', onMouseDown);
+		document.addEventListener('mouseup', onMouseUp);
+		document.addEventListener('mousemove', onMouseMove);
 	}
-	function onKeyup(event) {
-			if (event.keyCode == 32) freeze = false;
-	}
-	
 
 	document.addEventListener("keydown", onKeydown);
-	document.addEventListener("keyup", onKeyup);
 	// end controller section
-	
-
-	var speedRaw = 1;
-	var speedX = speedRaw / 600;
-	var speedY = 2 * speedRaw / 600;
-	var changeX = 0;
-	var changeY = 0;
-
 
 	function renderCurrent(currShader, currVertices, currIndices, option){
 		// Start using the context (analogy: start using the paints and the brushes)
@@ -366,6 +501,7 @@ function main() {
 		cur_program = option ;
 		if (option == 'l') gl.uniformMatrix4fv(uView, false, viewMatrix);
 		if (option == 'r') gl.uniformMatrix4fv(uViewR, false, viewMatrixR);
+		if (option == 'plane') gl.uniformMatrix4fv(uViewPlane, false, viewMatrixPlane);
 				
 		// Create a linked-list for storing the vertices data
 		var vertexBuffer = gl.createBuffer();
@@ -425,8 +561,9 @@ function main() {
 		var uNormalModel = gl.getUniformLocation(currShader, "uNormalModel");
 		gl.uniform3fv(uDiffuseConstant, [1.0, 1.0, 1.0]);   // white light
 		if (option == 'r')	gl.uniform3fv(uLightPositionR, box_center);    // light position
-		if (option == 'l')	gl.uniform3fv(uLightPosition, box_center);
-
+		if (option == 'l')	gl.uniform3fv(uLightPosition, box_center);    // light position
+		if (option == 'plane')	gl.uniform3fv(uLightPositionPlane, box_center);
+				
 		// Perspective projection
 		var uProjection = gl.getUniformLocation(currShader, "uProjection");
 		var perspectiveMatrix = glMatrix.mat4.create();
@@ -439,12 +576,19 @@ function main() {
 		gl.uniform3fv(uSpecularConstant, [1.0, 1.0, 1.0]);  // white light
 		gl.uniform3fv(uViewerPosition, [cameraX, cameraY, cameraZ]);
 		var uModel = gl.getUniformLocation(currShader, "uModel");
-
-		var modelMatrix = glMatrix.mat4.create();
-		gl.uniformMatrix4fv(uModel, false, modelMatrix);
-		var normalModelMatrix = glMatrix.mat3.create();
-		glMatrix.mat3.normalFromMat4(normalModelMatrix, modelMatrix);
-		gl.uniformMatrix3fv(uNormalModel, false, normalModelMatrix);
+		
+		// Transformation
+        var modelMatrix = glMatrix.mat4.create();
+        glMatrix.mat4.multiply(modelMatrix, modelMatrix, rotationMatrix);
+        gl.uniformMatrix4fv(uModel, false, modelMatrix);
+        var normalModelMatrix = glMatrix.mat3.create();
+        glMatrix.mat3.normalFromMat4(normalModelMatrix, modelMatrix);
+        gl.uniformMatrix3fv(uNormalModel, false, normalModelMatrix);
+		
+		if (challenge4switch) {
+			gl.uniform3fv(uDiffuseConstant, [.0, .0, .0]);   // white light / off
+			gl.uniform3fv(uSpecularConstant, [.0, .0, .0]);  // white light / off
+		}
 
 		gl.enable(gl.DEPTH_TEST);
 		//transparency func
@@ -468,7 +612,6 @@ function main() {
 		var primitive = gl.TRIANGLES;
 		var offset = 0;
 		var nVertex = currIndices.length;
-		// gl.drawArrays(primitive, offset, nVertex);
 		gl.drawElements(primitive, nVertex, gl.UNSIGNED_SHORT, offset);
 		cur_program = '';
 	}
@@ -477,6 +620,7 @@ function main() {
 		gl.clearColor(0., 0., 0., 1.); // black canvas
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 		// renderLeft();
+		renderCurrent(shaderProgramPlane, plane, indices_place, 'plane');
 		renderCurrent(shaderProgramL, vertices_left, indices_left, 'l');
 		renderCurrent(shaderProgramR, vertices, indices, 'r');
 		requestAnimationFrame(render);
